829f97a74a997453500f3085ca7b5d76b75732c5
Pravendra Kumar <pravendraksj7.com>
2023-01-24 10:16:36 +0530

    complte assissment

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,22 @@
 import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
+
 function App() {
+
+
+  const [para, setPara] = useState("");
+  console.log(para);
+
+   function updatePara(){
+    setPara(para +  "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy");
+    console.log(para);
+   }
+
   return (
     <div id="main">
-      // Do not alter the main div
+      <p id="para">{para}</p>
+      <button id ="click" onClick={updatePara}>add</button>
     </div>
   );
 }
